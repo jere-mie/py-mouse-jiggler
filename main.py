8dd14939c19b550ff6f952c4e085d19d3da38e60
jere-mie <jeremiedevelops@gmail.com>
2022-08-03 23:13:07 -0700

    added message to jiggler

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ except ImportError:
     print("pyautogui not installed - install it by running 'python3 -m pip install pyautogui' and re-run")
     exit(1)
 import time
+
+print("""
+########################################
+##                                    ##
+##  NOTE: to stop the mouse jiggler,  ##
+##  simply drag your mouse to the     ##
+##  top left of your screen very      ##
+##  quickly.                          ##
+##                                    ##
+########################################
+""")
+
 while True:
     pyautogui.dragTo(100, 100)
     time.sleep(0.5)
